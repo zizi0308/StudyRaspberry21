04b2d40bfa2fbc6d65bd9d4bbe01069b71e23873
JO HUI JI <whgmlwl222@naver.com>
2021-07-13 17:30:35 +0900

    20210713 flask활용

---
+++ flserver03.py
@@ -0,0 +1,13 @@
+# index.html 로딩서버
+from flask import Flask, render_template
+
+# Flask 객체 인스턴스 생성
+app = Flask(__name__)
+
+@app.route('/')  # 접속하는 최초 url
+def index():
+    # 백엔드에서 만든 데이터를 프론트엔드에 전달
+    return render_template('index.html', user='조희지', data={'userid' : 'zizi0308', 'gender' : 'female', 'age':26})
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080)
